4c0d5953a83b0ab466653e1405da18ba3456cc51
merscarlett <merliselison@gmail.com>
2026-03-13 22:41:23 +0200

    add vercel proxy

--- src/api/productsApi.ts
+++ src/api/productsApi.ts
@@ -1,15 +1,10 @@
-import type { Product } from "../types/product";
-
-export const getProducts = async (): Promise<Product[]> => {
-  const res = await fetch(
-    "https://ip-194-99-21-145-139178.vps.hosted-by-mvps.net/api/v1/products",
-  );
+export const getProducts = async () => {
+  const res = await fetch("/api/v1/products");
 
   if (!res.ok) {
     throw new Error("Failed to fetch products");
   }
 
   const data = await res.json();
-
   return data.data.products;
 };
