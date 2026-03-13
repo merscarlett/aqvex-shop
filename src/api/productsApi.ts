676758e3a9f182cc4864d65b8dba4d34973ace64
merscarlett <merliselison@gmail.com>
2026-03-13 22:35:49 +0200

    fix api url

--- src/api/productsApi.ts
+++ src/api/productsApi.ts
@@ -1,7 +1,9 @@
 import type { Product } from "../types/product";
 
 export const getProducts = async (): Promise<Product[]> => {
-  const res = await fetch("/api/v1/products");
+  const res = await fetch(
+    "https://ip-194-99-21-145-139178.vps.hosted-by-mvps.net/api/v1/products",
+  );
 
   if (!res.ok) {
     throw new Error("Failed to fetch products");
